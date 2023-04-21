69f02ceb813b4ee9da74ede5798f4686d59d4e4d
Allan2Fernandes <allan2fernandes@hotmail.com>
2023-04-21 10:32:51 +0200

    Initial commit

---
+++ AlsmatikBackend/Controllers/MainController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AlsmatikBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        [HttpGet("DBData/{UserID}")]
+        public async Task<ActionResult<List<object>>> DBConnectionTest(int UserID)
+        {
+            var handler = DbHandler.GetDbHandlerInstance();
+            Debug.WriteLine(UserID);
+
+            var data = handler.GetChosenParams(UserID, 3, 0); //2050 us
+
+            return Ok(data);
+        }
+    }
+}
---
+++ AlsmatikBackend/DbHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Data.SqlClient;
+
+namespace AlsmatikBackend
+{
+    public class DbHandler
+    {
+        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+        SqlConnection connection;
+        private static DbHandler instance = new DbHandler();
+        private DbHandler() // singleton constructor
+        {
+            builder.DataSource = "DESKTOP-2B3AJNE";
+            builder.UserID = "sa";
+            builder.Password = "123456";
+            builder.InitialCatalog = "PDHS_Guide";
+            connectToDB();
+        }
+
+        public static DbHandler GetDbHandlerInstance() //Singleton getter
+        {
+            return instance;
+        }
+
+        public void connectToDB()
+        {
+            connection = new SqlConnection(builder.ConnectionString);
+            connection.Open();
+        }
+
+        public List<object> GetChosenParams(int UserID, int ArtGrp, int ActualSetID) //2050, 3, 0
+        {
+            string sql = $"[dbo].[sp_GetFormParamUser] @UserID = {UserID},@ArtGrp = {ArtGrp}, @ActualSetID = {ActualSetID}";
+            SqlCommand command = new SqlCommand(sql, connection);
+            SqlDataReader reader = command.ExecuteReader();
+            var allData = new List<object>();
+            while (reader.Read())
+            {
+                var RowData = new
+                {
+                    ParamGroup = reader["ParamGroup"],
+                    ParamID = reader["ParamID"],
+                    ParText = reader["ParText"],
+                    ParUnit = reader["ParUnit"],
+                    ParComment = reader["ParComment"],
+                    ParValue = reader["ParValue"],
+                    Permission = reader["Permission"]
+                };
+                allData.Add(RowData);
+            }
+            reader.Close();
+            return allData;
+        }
+    }
+}
