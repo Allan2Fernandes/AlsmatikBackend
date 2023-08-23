37e4670f84229eb2cb03c73cf8330b277211ebc8
Allan2Fernandes <86521972+Allan2Fernandes@users.noreply.github.com>
2023-08-23 13:03:35 +0200

    Minor changes

--- AlsmatikBackend/Program.cs
+++ AlsmatikBackend/Program.cs
@@ -4,6 +4,8 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+      
+
         // Add services to the container.
 
         builder.Services.AddControllers();
